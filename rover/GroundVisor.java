ccc1b58dfdb62f3ae7fa3ea57bb9df1e456ae71c
Olga Kiseleva <kiseleva-1996@yandex.ru>
2017-03-12 21:40:50 +0300

    implements GroundVisor + checking Cell

--- rover/GroundVisor.java
+++ rover/GroundVisor.java
@@ -1,5 +1,30 @@
 package netcracker.intensive.rover;
 
+import netcracker.intensive.rover.constants.CellState;
+
 public class GroundVisor {
+    private int row_size;
+    private int column_size;
+
+    GroundVisor(int row_count, int column_count) {
+        row_size = row_count;
+        column_size = column_count;
+    }
+
+    // throws означает, что этот класс может найти исключения типа OutOfGroundException
+    // и только его может выбросить данный момент(незаявленные типы не может?)
+    CellState checkCell(GroundCell cell) throws OutOfGroundException {
+        //вспомогательные перемменные, чтобы не грузить if
+
+        int tmp_x = cell.getPoint().getX();
+        int tmp_y = cell.getPoint().getY();
+        Point tmp = new Point(tmp_x, tmp_y);
+        if ((tmp.getX() < 0) || (tmp.getX() > row_size) || (tmp.getY() < 0) || (tmp.getY() > column_size)) {
+            throw new OutOfGroundException("Ай,ай,ай ! Ты чуть не упал! Что же ты будешь без меня делать?");
+        } else {
+            return cell.getState();
+        }
+
+    }
 
 }
--- rover/OutOfGroundException.java
+++ rover/OutOfGroundException.java
@@ -3,5 +3,12 @@ package netcracker.intensive.rover;
 /**
  * Это исключение должно быть проверяемым - оно возникает, если ровер вышел за пределы поверхности
  */
-public class OutOfGroundException {
+public class OutOfGroundException extends Exception {
+
+    OutOfGroundException(String message) {
+        //возможно, сюда добавить взлет
+        super(message);
+    }
+
+
 }
