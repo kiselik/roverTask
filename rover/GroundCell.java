e44c4eaee244cbcdf60911dcb84ad717a661d33f
Olga Kiseleva <kiseleva-1996@yandex.ru>
2017-03-12 20:58:41 +0300

    implements class GroundCell

--- rover/GroundCell.java
+++ rover/GroundCell.java
@@ -1,5 +1,25 @@
 package netcracker.intensive.rover;
 
+import netcracker.intensive.rover.constants.CellState;
+
 public class GroundCell {
 
+    private Point current_point;
+    private CellState state;
+
+    GroundCell(CellState cur_state) {
+        state = cur_state;
+    }
+
+    void SetPoint(int cur_x, int cur_y) {
+        current_point = new Point(cur_x, cur_y);
+    }
+
+    CellState getState() {
+        return state;
+    }
+
+    Point getPoint() {
+        return current_point;
+    }
 }
