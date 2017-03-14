a90f9b1407d7caa67553f0fc3c3ff6427d12cadf
Olga Kiseleva <kiseleva-1996@yandex.ru>
2017-03-14 21:31:28 +0300

    new methods and fields for RoverProgram

--- rover/programmable/RoverProgram.java
+++ rover/programmable/RoverProgram.java
@@ -6,11 +6,19 @@ public class RoverProgram {
     public static final String LOG = "log";
     public static final String STATS = "stats";
     public static final String SEPARATOR = "===";
+    public static final String TURN = "turn";
+    public static final String MOVE= "move";
+    public static final String LIFT = "lift";
+    public static final String LAND = "land";
 
 
     private HashMap<String, Object> config = new HashMap<>();
 
-    HashMap<String, Object> getCongig() {
+    void memoriseConfig(String Key, Object Value){
+        config.put(Key,Value);
+
+    }
+    HashMap<String, Object> getConfig() {
         return config;
     }
 }
