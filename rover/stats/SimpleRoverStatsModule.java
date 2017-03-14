030a8589775b93756e0283895375d70935ca1dea
Olga Kiseleva <kiseleva-1996@yandex.ru>
2017-03-14 21:32:38 +0300

    implements Stats module

--- rover/stats/SimpleRoverStatsModule.java
+++ rover/stats/SimpleRoverStatsModule.java
@@ -1,5 +1,31 @@
 package netcracker.intensive.rover.stats;
 
+import netcracker.intensive.rover.Point;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class SimpleRoverStatsModule implements RoverStatsModule {
+    private ArrayList<Point> visitedCells;
+
+    SimpleRoverStatsModule() {
+        visitedCells = new ArrayList<Point>();
+    }
+
+    @Override
+    public void registerPosition(Point position) {
+        if (!visitedCells.contains(position))
+            visitedCells.add(position);
+    }
+
+    @Override
+    public boolean isVisited(Point point) {
+        return visitedCells.contains(point);
+    }
+
+    @Override
+    public Collection<Point> getVisitedPoints() {
+        return visitedCells;
+    }
 
 }
