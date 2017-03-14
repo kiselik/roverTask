f590f0158400cb6ad8c062fb0244fecde9a42fe8
Olga Kiseleva <kiseleva-1996@yandex.ru>
2017-03-14 22:06:36 +0300

    implements LandCommand

--- rover/command/LandCommand.java
+++ rover/command/LandCommand.java
@@ -1,8 +1,22 @@
 package netcracker.intensive.rover.command;
 
+import netcracker.intensive.rover.Point;
+import netcracker.intensive.rover.Rover;
+import netcracker.intensive.rover.constants.Direction;
+
 public class LandCommand implements RoverCommand {
+    private Rover rover;
+    private Point position;
+    private Direction direction;
+
+    LandCommand(Rover cur_rover, Point cur_position, Direction cur_direction) {
+        rover = cur_rover;
+        position = cur_position;
+        direction = cur_direction;
+    }
+
     @Override
     public void execute() {
-
+        rover.land(position, direction);
     }
 }
