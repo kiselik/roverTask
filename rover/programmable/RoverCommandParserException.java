32665d236ecaffe65ae182e4119e836af511287f
Olga Kiseleva <kiseleva-1996@yandex.ru>
2017-03-14 13:39:27 +0300

    added new Exception

--- rover/programmable/RoverCommandParserException.java
+++ rover/programmable/RoverCommandParserException.java
@@ -4,5 +4,12 @@ package netcracker.intensive.rover.programmable;
  * Это исключение должно быть непроверяемым - оно происходит, когда возникает ошибка чтения файла с программой для ровера.
  * Дальнейшее выполнение программы становится невозможным. Исключение должно содержать информации о породившем его исключении.
  */
-public class RoverCommandParserException {
+import java.io.IOException;
+
+public class RoverCommandParserException extends Exception{
+    RoverCommandParserException(Exception e) {
+        super("возникли проблемы при работе с файлом",e);
+        printStackTrace();
+        System.exit(-1);
+    }
 }
