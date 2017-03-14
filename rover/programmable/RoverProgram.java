5fbb7d94967846dbbfba4399567a0f2e656ca926
Olga Kiseleva <kiseleva-1996@yandex.ru>
2017-03-14 14:02:55 +0300

    added Hash for congig

--- rover/programmable/RoverProgram.java
+++ rover/programmable/RoverProgram.java
@@ -1,14 +1,16 @@
 package netcracker.intensive.rover.programmable;
 
-import netcracker.intensive.rover.command.RoverCommand;
-
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
 
 public class RoverProgram {
     public static final String LOG = "log";
     public static final String STATS = "stats";
     public static final String SEPARATOR = "===";
+
+
+    private HashMap<String, Object> config = new HashMap<>();
+
+    HashMap<String, Object> getCongig() {
+        return config;
+    }
 }
