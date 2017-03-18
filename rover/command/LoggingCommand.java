4e69ad2628dfe7bf6a992acbf5fb1f992da1324b
Olga Kiseleva <kiseleva-1996@yandex.ru>
2017-03-19 01:30:29 +0300

    added logging

--- rover/command/LoggingCommand.java
+++ rover/command/LoggingCommand.java
@@ -4,11 +4,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LoggingCommand implements RoverCommand {
+    private RoverCommand log_command;
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingCommand.class);
+    public static final Logger LOGGER = LoggerFactory.getLogger(LoggingCommand.class);
 
+    public LoggingCommand(RoverCommand command) {
+        log_command=command;
+    }
     @Override
     public void execute() {
+        LOGGER.info(log_command.toString());
+        log_command.execute();
+    }
+
+    @Override
+    public String toString() {
+        return log_command.toString();
 
     }
 }
